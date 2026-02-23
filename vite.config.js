baec210ca298ba33b0c8e0d19504d58b05b75439
izzaqr <amirkazanfarov@gmail.com>
2026-02-23 15:38:22 +0300

    add image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        ViteImageOptimizer({
+            includePublic: true,
+            jpg: { quality: 80 },
+            png: { quality: 80 },
+            webp: { quality: 80 },
+            convertToWebp: {
+                jpg: true,
+                png: true,
+            },
+        }),
+    ],
     base: "/tokyo05/",
 })
