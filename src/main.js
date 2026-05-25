95ede9c284204a71c72c5c641beb4f1ba849a2ef
izzaqr <amirkazanfarov@gmail.com>
2026-05-25 06:04:35 +0300

    add tg tma

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from "vue"
 import "./assets/reset.css"
 import App from "./App.vue"
 
+const tg = window.Telegram.WebApp
+tg.ready() // сообщает Telegram, что приложение загрузилось
+tg.expand() // раскрывает на весь экран
+
 createApp(App).mount("#app")
